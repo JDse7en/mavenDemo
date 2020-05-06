b197d6798ae32e09cb47d6dd57e1b630c001e8e5
JDse7en <59498617+JDse7en@users.noreply.github.com>
2020-05-06 15:26:28 +0800

    Update App.java

--- src/main/java/com/songqin/mavenDemo/App.java
+++ src/main/java/com/songqin/mavenDemo/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "HHHHello World!" );
+        System.out.println( "Hello World!" );
         System.out.println("first update");
     }
 }
